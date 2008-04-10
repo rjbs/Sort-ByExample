9a9a1f94dd91dced07d4e0c6cfc8a7e151c5a059
Ricardo SIGNES <rjbs@cpan.org>
2008-04-09 22:22:41 -0400

    add author, abstract

--- lib/Sort/ByExample.pm
+++ lib/Sort/ByExample.pm
@@ -9,11 +9,11 @@ Sort::ByExample - sort lists to look like the example you provide
 
 =head1 VERSION
 
-version 0.003
+version 0.004
 
 =cut
 
-our $VERSION = '0.003';
+our $VERSION = '0.004';
 
 =head1 SYNOPSIS
 
